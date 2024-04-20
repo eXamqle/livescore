54e0700eef658a5de2984c2de4a92e0477c476d7
Luka <kuznikluka@gmail.com>
2024-04-20 12:49:16 +0000

    Add a first test for the retrieval of the data from the server for match service

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// custom imports
+import { MatchService } from './services/match.service';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('MatchService', () => {
+	it('fetches match data from the server', async () => {
+	  const service = new MatchService();
+	  const data = await service.getMatches();
+	  expect(data).toBeDefined();
+	});
+  });
