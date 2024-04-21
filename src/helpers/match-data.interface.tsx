cdeefb8f3c8eac608edc1ccf70b2d599216acfca
Luka <kuznikluka@gmail.com>
2024-04-21 17:02:34 +0000

    Add phase type strings to the interface file

--- src/helpers/match-data.interface.tsx
+++ src/helpers/match-data.interface.tsx
@@ -2,8 +2,10 @@ import { Team } from '../helpers/team.interface'
 import { Match } from '../helpers/match.interface'
 import { Event } from '../helpers/event.interface'
 
+export type phaseType = 'pre_match' | 'match' | 'post_match'
+
 export interface MatchData {
-	phase: string;
+	phase: phaseType;
 	teams: Team[];
 	matches: Match[];
 	events: Event[];
