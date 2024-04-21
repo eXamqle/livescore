eedca0f03646218a7edac287a3951af8b6cfebae
Luka <kuznikluka@gmail.com>
2024-04-21 16:28:03 +0000

    Render an error text if there is an error retreiving the data from the server

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 // custom imports
 import { MatchService } from './services/match.service';
 import { Scoreboard } from '../src/components/scoreboard.component';
-
-test('renders without crashing', () => {
-	render(<Scoreboard />);
-});
 
 describe('MatchService', () => {
 	it('fetches match data from the server', async () => {
@@ -15,4 +11,14 @@ describe('MatchService', () => {
 		const data = await service.getMatches();
 		expect(data).toBeDefined();
 	});
+});
+
+it('renders error message when there is an error', () => {
+	MatchService.prototype.getMatches = jest.fn(() => Promise.reject(new Error('Test error')));
+
+	render(<Scoreboard />);
+
+	setTimeout(() => {
+		expect(screen.getByText('There was an error while retrieving the data from the server')).toBeInTheDocument();
+	}, 2000);
 });
