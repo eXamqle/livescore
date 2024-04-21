dff73262918768f7dd2a2f35ecaed30b6fa4a649
Luka <kuznikluka@gmail.com>
2024-04-21 15:14:08 +0000

    Define all event types in the interface

--- src/helpers/event.interface.tsx
+++ src/helpers/event.interface.tsx
@@ -1,6 +1,8 @@
+export type EventType = 'match_start' | 'goal' | 'match_halftime' | 'match_end';
+
 export interface Event {
 	event_id: number;
-	event_type: string;
+	event_type: EventType;
 	event_time: number;
 	match_id: number;
 	score_amount?: number;
