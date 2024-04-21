8a7f362140895fde049f44dcc0b6ca87755aa051
Luka <kuznikluka@gmail.com>
2024-04-21 16:28:35 +0000

    Test rendering of the component if it has the right data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -22,3 +22,32 @@ it('renders error message when there is an error', () => {
 		expect(screen.getByText('There was an error while retrieving the data from the server')).toBeInTheDocument();
 	}, 2000);
 });
+
+it('renders matches when matchData has data', () => {
+	MatchService.prototype.getMatches = jest.fn(() => Promise.resolve({
+		"phase": "match",
+		"teams": [
+			{
+				"team_id": 8982001,
+				"team_name": "VL Lisbon",
+				"team_name_short": "LIS"
+			}        
+		],
+		"matches": [
+			{
+				"match_id": 251071404,
+				"tournament_id": 1775490,
+				"round": 18,
+				"home_team_id": 8982008,
+				"away_team_id": 8982001
+			}
+		],
+		"events": []
+	}));
+
+	render(<Scoreboard />);
+
+	setTimeout(() => {
+		expect(screen.getByText('Test Team 1')).toBeInTheDocument();
+	}, 2000);
+});
